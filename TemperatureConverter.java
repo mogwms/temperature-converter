3330f9e76c3f22645b1e68ae9797966b5cb73c8f
Malcolm Williams <mogwms@gmail.com>
2025-10-20 12:47:37 -0400

    Create TemperatureConverter.java

---
+++ TemperatureConverter.java
@@ -0,0 +1,12 @@
+public class TemperatureConverter {
+public static void main(String[] args) {
+
+double celcius = 35.5;
+double fh = celcius * 9 / 5 + 32;
+
+System.out.print("   ");
+System.out.print("Celcius: " + celcius);
+System.out.print("   ");
+System.out.print("Fahrenheit: "+ fh);
+    }
+}
